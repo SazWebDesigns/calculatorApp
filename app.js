05babb2607d8a418014dfe5ba457c8da6303c19f
Saz <sazwebdesigns@gmail.com>
2017-05-12 19:57:48 +0100

    Created a createButtons function Creates the following buttons and adds them to main div: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, +, -, /, *, ., =

--- app.js
+++ app.js
@@ -5,7 +5,37 @@ function createElement(type, attr, value){
     return element;
 }
 
-calculator  = createElement('div', 'class', 'container');
+function createButtons(parent){
+    buttons = [createElement('buttons', 'textContent', '1'),
+                createElement('buttons', 'textContent', '2'),
+                createElement('buttons', 'textContent', '3'),
+                createElement('buttons', 'textContent', '4'),
+                createElement('buttons', 'textContent', '5'),
+                createElement('buttons', 'textContent', '6'),
+                createElement('buttons', 'textContent', '7'),
+                createElement('buttons', 'textContent', '8'),
+                createElement('buttons', 'textContent', '9'),
+                createElement('buttons', 'textContent', '0'),
+                createElement('buttons', 'textContent', '+'),
+                createElement('buttons', 'textContent', '-'),
+                createElement('buttons', 'textContent', '/'),
+                createElement('buttons', 'textContent', '*'),
+                createElement('buttons', 'textContent', '.'),
+                createElement('buttons', 'textContent', '=')
+    ];
+    for(let i = 0; i < buttons.length; i++){
+        buttons[i].className = "text-center btn btn-primary"
+        parent.appendChild(buttons[i]);
+    }
+
+}
+
+
+calculator  = createElement('div', 'className', 'container');
+calculator.style.width = '400px';
+calculator.style.height = '600px';
+calculator.style.backgroundColor = 'lightgray';
+createButtons(calculator);
 body = document.querySelector('body');
 scriptTags = body.children;
 body.insertBefore(calculator, scriptTags[0]);
