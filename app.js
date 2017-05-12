f1e14a70c747095f4421e8abaf55e7bbf403f302
Saz <sazwebdesigns@gmail.com>
2017-05-12 20:12:12 +0100

    Added buttons in a normal order

--- app.js
+++ app.js
@@ -18,16 +18,41 @@ function createButtons(parent){
                 createElement('buttons', 'textContent', '0'),
                 createElement('buttons', 'textContent', '+'),
                 createElement('buttons', 'textContent', '-'),
-                createElement('buttons', 'textContent', '/'),
                 createElement('buttons', 'textContent', '*'),
+                createElement('buttons', 'textContent', '/'),
                 createElement('buttons', 'textContent', '.'),
                 createElement('buttons', 'textContent', '=')
     ];
     for(let i = 0; i < buttons.length; i++){
-        buttons[i].className = "text-center btn btn-primary"
-        parent.appendChild(buttons[i]);
+        buttons[i].className = "text-center btn btn-primary col-xs-3"
     }
+    row1 = createElement('div', 'className', 'row col-xs-12');
+    row1.appendChild(buttons[9]);
+    row1.appendChild(buttons[14]);
+    row1.appendChild(buttons[15]);
+    row1.appendChild(buttons[10]);
+    parent.appendChild(row1);
 
+    row2 = createElement('div', 'className', 'row col-xs-12');
+    row2.appendChild(buttons[0]);
+    row2.appendChild(buttons[1]);
+    row2.appendChild(buttons[2]);
+    row2.appendChild(buttons[11]);
+    parent.insertBefore(row2, row1);
+
+    row3 = createElement('div', 'className', 'row col-xs-12');
+    row3.appendChild(buttons[3]);
+    row3.appendChild(buttons[4]);
+    row3.appendChild(buttons[5]);
+    row3.appendChild(buttons[12]);
+    parent.insertBefore(row3, row2);
+
+    row4 = createElement('div', 'className', 'row col-xs-12');
+    row4.appendChild(buttons[6]);
+    row4.appendChild(buttons[7]);
+    row4.appendChild(buttons[8]);
+    row4.appendChild(buttons[13]);
+    parent.insertBefore(row4, row3);
 }
 
 
